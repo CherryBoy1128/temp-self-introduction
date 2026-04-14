d5ef701c84a3aa24be6787cb96b22235fe697a1d
CherryBoy1128 <syyooo@gmail.com>
2026-04-14 15:55:02 +0900

    修正したよ

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < div className="App">
+      <div className="top-section">
+        <div className="display">
+          <div className="myPhoto"></div>
+          <div>
+            <div className="name"></div>     {/* ここに名前を書いてください*/}
+            <div className="oneWord"></div>  {/* なにか一言あれば書いてください*/}
+          </div>
+        </div>
+      </div>
+
+      <div className="myself">
+        <div className="profile-row">
+          <div className="label">学科</div>
+          <div className="myDepartment"></div>   {/* あなたの所属している学科を書いてください*/}
+        </div>
+
+        <div className="profile-row">
+          <div className="label">誕生日 </div>
+          <div className="myBirthday"></div>     {/* あなたの誕生日を書いてください*/}
+        </div>
+
+        <div className="profile-row">
+          <div className="label">在住</div>
+          <div className="myFrom"></div>         {/* あなたの住んでいるところを書いてください（あまり言いたくなかったら都道府県とか、なんなら日本語でもいいです）*/}
+        </div>
+
+        <div className="profile-row">
+          <div className="label"></div>
+          <div className=""></div>       
+        </div>
+
+        <div className="profile-row">
+          <div className="label"></div>
+          <div className=""></div>
+        </div>
+
+        <div className="profile-row">
+          <div className="label"></div>
+          <div className=""></div>
+        </div>
+
+        <div className="profile-row">  
+          <div className="label"></div>
+          <div className=""></div>
+        </div>
+
+      </div>
     </div>
   );
 }
